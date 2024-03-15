8414b126edcae4f24a93563dffe34e69dcf9e52e
cuifan <cuifan@isv-tech.com>
2024-03-15 14:13:59 +0800

    feat(二叉树): leetcode 429. N叉树的层序遍历

---
+++ 06-二叉树/429. N 叉树的层序遍历.js
@@ -0,0 +1,99 @@
+class Node {
+    constructor(val) {
+        this.next = null;
+        this.prev = null;
+        this.val = val;
+    }
+}
+
+class MyQueue {
+    #size = 0;
+    constructor(val) {
+        this.head = null;
+        this.tail = null;
+        if (val !== undefined) {
+            const node = new Node(val);
+            this.head = node;
+            this.tail = node;
+            this.#size++;
+        }
+    }
+
+    pop() {
+        if (this.empty()) {
+            return null;
+        }
+        const ans = this.head.val;
+        this.head = this.head.prev;
+        if (this.head) {
+            this.head.next = null;
+        }
+        this.#size--;
+        return ans;
+    }
+
+    push(val) {
+        const node = new Node(val);
+        if (!this.head) {
+            this.head = node;
+        }
+        if (this.tail) {
+            this.tail.prev = node;
+        }
+        node.next = this.tail;
+        this.tail = node;
+        this.#size++;
+    }
+
+    peek() {
+        if (!this.head) {
+            return null;
+        }
+        return this.head.val;
+    }
+
+    size() {
+        return this.#size;
+    }
+
+    empty() {
+        return this.#size === 0;
+    }
+}
+
+
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    const ans = [];
+    const queue = new MyQueue();
+    if (root) queue.push(root);
+    while (!queue.empty()) {
+        const size = queue.size();
+        const results = [];
+        for (let i = 0; i < size; i++) {
+            const popNode = queue.pop();
+            results.push(popNode.val);
+
+            if (!popNode.children) {
+                continue;
+            }
+
+            for (let childNode of popNode.children) {
+                childNode && queue.push(childNode);
+            }
+        }
+        ans.push(results);
+    }
+    return ans;
+};
