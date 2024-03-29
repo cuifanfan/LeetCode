0bf4bf048ab2cf1e47e2097b8f83aa1c4730b22f
cuifan <cuifan@isv-tech.com>
2024-03-29 18:00:29 +0800

    feat(二叉树): leetcode 257.二叉树的所有路径
    增加迭代写法，第一次学习到了回溯

--- 06-二叉树/257. 二叉树的所有路径.js
+++ 06-二叉树/257. 二叉树的所有路径.js
@@ -28,6 +28,102 @@ var binaryTreePaths = function (root) {
 };
 
 
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function (root) {
+    const result = [];
+    function treePath(node, path = '', result = []) {
+        path += node.val;
+        if (node.left === null && node.right === null) {
+            result.push(path);
+            return;
+        }
+
+        if (node.left) {
+            path += '->';
+            treePath(node.left, path, result);
+            // 回溯掉 '->'
+            path = path.slice(0, path.length - 2);
+        }
+        if (node.right) {
+            path += '->';
+            treePath(node.right, path, result);
+            // 回溯掉 '->'
+            path = path.slice(0, path.length - 2);
+        }
+    }
+    treePath(root, '', result);
+    return result;
+};
+
+
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function (root) {
+    if (root === null) {
+        return [];
+    }
+    const result = [];
+    const stack = [root];
+    const pathMap = new Map();
+    while (stack.length !== 0) {
+        const orderNode = stack.pop();
+        if (orderNode === null) {
+            // 访问节点
+            const accessNode = stack.pop();
+            const prePath = pathMap.has(accessNode) ? (pathMap.get(accessNode) + '->') : '';
+            const path = prePath + accessNode.val;
+            accessNode.left && pathMap.set(accessNode.left, path);
+            accessNode.right && pathMap.set(accessNode.right, path);
+            !accessNode.left && !accessNode.right && result.push(path);
+            continue;
+        }
+        // 确定访问顺序
+        orderNode.right && stack.push(orderNode.right);
+        orderNode.left && stack.push(orderNode.left);
+        stack.push(orderNode, null);
+    }
+    return result;
+};
+
+
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function (root) {
+    if (root === null) {
+        return [];
+    }
+    const result = [];
+    const stack = [root];
+    const pathStack = [];
+    while (stack.length !== 0) {
+        const orderNode = stack.pop();
+        if (orderNode === null) {
+            // 访问节点
+            const accessNode = stack.pop();
+            const prePath = pathStack.length ? pathStack[pathStack.length - 1] + '->' : '';
+            const path = prePath + accessNode.val;
+            if (accessNode.left === null && accessNode.right === null) {
+                result.push(path);
+            } 
+            continue;
+        }
+        // 确定访问顺序
+        orderNode.right && stack.push(orderNode.right);
+        orderNode.left && stack.push(orderNode.left);
+        stack.push(orderNode, null);
+    }
+    console.log(pathStack);
+    return result;
+};
+
+
 // 创建二叉树
 function TreeNode(val, left, right) {
     this.val = (val === undefined ? 0 : val)
